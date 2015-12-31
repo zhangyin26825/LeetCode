9e0665740536dc6e41ad1708901f7e9f34a86e5d
张银 <459402930@qq.com>
2015-12-31 10:01:46 +0800

    正则匹配

--- src/main/java/com/zhangyin/leetcode/RegularExpressionMatching.java
+++ src/main/java/com/zhangyin/leetcode/RegularExpressionMatching.java
@@ -23,11 +23,19 @@ isMatch("ab", ".*") → true
 isMatch("aab", "c*a*b") → true
 Subscribe to see which companies asked this question
  * @author yin
- *
+ *"aaa"
+"ab*a*c*a"  
+"bbbba"
+".*a*a"
  */
 public class RegularExpressionMatching {
 	public static void main(String[] args) {
-		System.out.println(isMatch("aa", "a*"));
+//		System.out.println(isMatch("c", "a*c"));
+//		System.out.println(isMatch("ab", ".*"));
+//		System.out.println(isMatch("aa", ".*"));
+//		System.out.println(isMatch("aaa","aa"));
+//		System.out.println(isMatch("a", "ab*"));
+		System.out.println(isMatch("bbbba", ".*a*a"));
 	}
 	
 	public static boolean isMatch(String s, String p) {
@@ -36,15 +44,23 @@ public class RegularExpressionMatching {
 		}
 		boolean [] [] r=new boolean[p.length()+1][s.length()+1];
 		r[0][0]=true;
-		for (int i = 0; i < p.length(); i++) {	
-			for (int j = 0; j < s.length(); j++) {
+		for (int i = 0; i <= p.length(); i++) {	
+			for (int j = 0; j <= s.length(); j++) {
 				if(i==0||j==0){
 					continue;
 				}
 				char pi = p.charAt(i-1);
 				char sj = s.charAt(j-1);
+				if(j==s.length()&&r[i-1][j]){
+					if(pi!='*'&&pi!=sj&&i<p.length()&&p.charAt(i)=='*'){
+						r[i][j]=true;
+					}
+					if(pi=='*'){
+						 r[i][j]=true;	
+					}
+				}
 				if(r[i-1][j-1]){
-					if(pi!='*'&&pi==sj||pi=='.'){
+					if(pi!='*'&&(pi==sj||pi=='.')){
 						r[i][j]=true;
 						continue;
 					}
@@ -53,16 +69,18 @@ public class RegularExpressionMatching {
 						continue;
 					}
 					if(pi=='*'){
-						 char c=p.charAt(i-2);
-						 for (int k = j-1; k < s.length(); k++) {
-							if(c==s.charAt(k)||c=='.'){
-								r[i][k+1]=true;
-							}else{
-								break;
-							}
-						}
+							char c=p.charAt(i-2);
+							for (int k = j-1; k < s.length(); k++) {
+								if(c==s.charAt(k)||c=='.'){
+									r[i][k+1]=true;
+								}else{
+									break;
+								}
+							 }
+							 r[i][j-1]=true;	 
 					}
 				}
+				
 				
 			}
 		}
