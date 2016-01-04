8cf779dafb89cc6e917f1b04549bbe4b0404f807
张银 <459402930@qq.com>
2016-01-04 09:24:16 +0800

    fds

--- src/main/java/com/zhangyin/leetcode/RegularExpressionMatching.java
+++ src/main/java/com/zhangyin/leetcode/RegularExpressionMatching.java
@@ -30,54 +30,68 @@ Subscribe to see which companies asked this question
  */
 public class RegularExpressionMatching {
 	public static void main(String[] args) {
-//		System.out.println(isMatch("c", "a*c"));
-//		System.out.println(isMatch("ab", ".*"));
-//		System.out.println(isMatch("aa", ".*"));
-//		System.out.println(isMatch("aaa","aa"));
-//		System.out.println(isMatch("a", "ab*"));
+		System.out.println(isMatch("c", "a*c"));
+		System.out.println(isMatch("ab", ".*"));
+		System.out.println(isMatch("aa", ".*"));
+		System.out.println(isMatch("aaa","aa"));
+		System.out.println(isMatch("a", "ab*"));
 		System.out.println(isMatch("bbbba", ".*a*a"));
+		System.out.println(isMatch("a", ".*"));
+		System.out.println(isMatch("", "c*"));
+		System.out.println(isMatch("aabccbcbacabaab", ".*c*a*b.*a*ba*bb*"));
 	}
 	
 	public static boolean isMatch(String s, String p) {
 		if(s.equals(p)){
 			return true;
 		}
+		if(s.length()==0){
+			for (int i = 0; i < p.length(); i++){
+					if(p.charAt(i)=='*'){
+						continue;
+					}
+					if(p.charAt(i)!='*'&&i+1<p.length()&&p.charAt(i+1)=='*'){
+						continue;
+					}else{
+						return false;
+					}	
+			}
+			return true;
+		}
+		
 		boolean [] [] r=new boolean[p.length()+1][s.length()+1];
 		r[0][0]=true;
 		for (int i = 0; i <= p.length(); i++) {	
+			//i 为 p上的char的索引 -1
 			for (int j = 0; j <= s.length(); j++) {
-				if(i==0||j==0){
+				//j 为 s上的char的索引 -1
+				if(i==0&&j==0){
+					r[0][0]=true;
+					continue;
+				}	
+				if(i==0){
+					continue;
+				}
+				if(j==0&&p.charAt(i-1)!='*'){
 					continue;
 				}
 				char pi = p.charAt(i-1);
-				char sj = s.charAt(j-1);
-				if(j==s.length()&&r[i-1][j]){
-					if(pi!='*'&&pi!=sj&&i<p.length()&&p.charAt(i)=='*'){
-						r[i][j]=true;
-					}
-					if(pi=='*'){
-						 r[i][j]=true;	
-					}
-				}
-				if(r[i-1][j-1]){
-					if(pi!='*'&&(pi==sj||pi=='.')){
-						r[i][j]=true;
-						continue;
-					}
-					if(pi!='*'&&pi!=sj&&i<p.length()&&p.charAt(i)=='*'){
-						r[i][j-1]=true;
-						continue;
-					}
-					if(pi=='*'){
-							char c=p.charAt(i-2);
-							for (int k = j-1; k < s.length(); k++) {
-								if(c==s.charAt(k)||c=='.'){
-									r[i][k+1]=true;
-								}else{
-									break;
-								}
-							 }
-							 r[i][j-1]=true;	 
+				if(pi!='*'){//这里判断 * 是因为  * 是唯一会产生多长度判断的元素  .只需要判断一个
+					// pi 当前值不为  　*
+					char sj = s.charAt(j-1);
+					r[i][j]=r[i-1][j-1]&&(pi==sj||pi=='.');
+					
+				}else{
+					// pi 当前值为  　*
+					if(r[i-2][j]){
+						r[i][j]=r[i-2][j];
+						char c = p.charAt(i-2);
+						int k=j+1;
+						while(k-1<s.length()&&(s.charAt(k-1)==c||c=='.')){
+							r[i][k]=true;
+							k++;
+						}
+						
 					}
 				}
 				
